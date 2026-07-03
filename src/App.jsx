4548173188c85c74e728145e31ca254c70519d4b
gtrpaulj-creator <gtrpaulj@gmail.com>
2026-07-03 15:00:37 -0700

    Update App.jsx
    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Music from './components/Music.jsx'
 import Shows from './components/Shows.jsx'
-import Performances from './components/Performances.jsx'
 import Contact from './components/Contact.jsx'
 import Footer from './components/Footer.jsx'
 
@@ -16,7 +15,6 @@ export default function App() {
         <About />
         <Music />
         <Shows />
-        <Performances />
         <Contact />
       </main>
       <Footer />
